ffa3d42c5f994aeb1dabb4b579957cba68baf985
szk07 <on.szk07@gmail.com>
2018-02-18 19:14:57 +0900

    add: Contactのフォーム送信内容をDBに登録

--- php/SendForm.php
+++ php/SendForm.php
@@ -6,6 +6,17 @@ $name = es($_POST['Name']);
 $msg = es($_POST['Message']);
 $to = es($_POST['Email']);
 
+try{
+ $db = connect();
+ $sql = 'INSERT INTO ContactForm(Name, Email, Message, time)
+                VALUES (:name, :mail, :msg, NOW())';
+ $sth = $db->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+ $sth->execute(array(':name' => $name, ':mail' => $to, ':msg' => $msg));
+ $db = NULL;
+}catch(PDOException $e){
+ exit("エラーが発生しました。：{$e->getMessage()}");
+}
+
 require_once 'SendForm_mail.php';
 $admin = mb_send_mail($to, $subject, $body, $header);
 
